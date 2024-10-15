a1fe7db870ab4d5c038db5eb21f98c522ffbaea7
Juan Cabello <jcabelloc@itana.pe>
2024-10-14 20:02:53 -0500

    Codigo Inicial de la Sesion

---
+++ app.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const adminRoutes = require('./routes/admin')
+const tiendaRoutes = require('./routes/tienda')
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/admin', adminRoutes);
+app.use(tiendaRoutes);
+
+app.use((req, res, next) => {
+    // res.status(404).send('<h1>PAGINA NO ENCONTRADA</h1>');
+    console.log('Viendo que URL el usuario inhresa pero no existe')
+    console.log(req.url);
+    next();
+});
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
+
+app.listen(3000);
